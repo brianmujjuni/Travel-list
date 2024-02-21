f4bb9e7a71d0e4c29342d41834d3d452893a3a64
brianmujjuni <brianmujjuni@engineer.com>
2024-02-21 16:46:26 +0300

    add derived state for nun items

--- src/App.js
+++ src/App.js
@@ -100,10 +100,14 @@ function Item({ item, onDeleteItem,onToggleItem }) {
 function Stats({items}) {
   const numItems = items.length
   const numPacked = items.filter(item=>item.packed).length
-  const percentage = Math.round(numPacked / numItems * 100)
+  const percentage = Math.round((numPacked / numItems) * 100)
   return (
     <footer className="stats">
-      <em>You have {numItems} items on your list,and you already packed {numPacked} ({percentage}%)</em>
+      <em>
+        { percentage === 100 ? 'You got everything Ready' :
+        `You have ${numItems} items on your list,and you already packed ${numPacked} (${percentage}%)`
+        }
+        </em>
     </footer>
   );
 }
