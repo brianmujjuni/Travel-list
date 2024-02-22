6ec95a20b93c50c843b0f6dffef2276e4d1759a5
brianmujjuni <brianmujjuni@engineer.com>
2024-02-22 10:27:11 +0300

    add sort by description

--- src/App.js
+++ src/App.js
@@ -78,6 +78,8 @@ function PackingList({ items, onDeleteItem,onToggleItem }) {
   const [sortBy,setSortBy] = useState('input')
   let sortedItems;
   if(sortBy === 'input') sortedItems = items 
+  if(sortBy ==='description') sortedItems = 
+                items.slice().sort((a,b)=> a.description.localeCompare(b.description))
   return (
     <div className="list">
       <ul>
